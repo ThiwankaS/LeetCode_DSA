3ebc819acc0e0e3164de869a16519898b649daaa
thiwankas <thivankas@outlook.com>
2024-10-09 06:08:46 +0300

    Split array solution : with out additional arrays

--- Arrays_Strings/spliitArray.c
+++ Arrays_Strings/spliitArray.c
@@ -2,16 +2,14 @@
 
 int waysToSplitArray(int *nums, int numsSize)
 {
-    long int prefix[numsSize];
-    int i = 0, count = 0, j = numsSize - 1;
-    prefix[i] = nums[i];
-    for (i = 1; i < numsSize; i++)
-    {
-        prefix[i] = nums[i] + prefix[i - 1];
-    }
+    int count = 0;
+    long int total = 0, curr = 0;
+    for (int i = 0; i < numsSize; i++)
+        total = total + nums[i];
     for (int k = 0; k < numsSize - 1; k++)
     {
-        if (prefix[k] >= (prefix[numsSize - 1] - prefix[k]))
+        curr = curr + nums[k];
+        if (curr >= (total - curr))
             count++;
     }
     return (count);
@@ -19,7 +17,7 @@ int waysToSplitArray(int *nums, int numsSize)
 
 int main(void)
 {
-    int nums[] = {0, 0};
+    int nums[] = {10, 4, -8, 7};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
     printf("%d\n", waysToSplitArray(nums, numsSize));
     return (0);
