9cfc3d09c7c21456f7e4a31541902fac1f97d5d0
thiwankas <thivankas@outlook.com>
2024-10-09 06:00:59 +0300

    Split array solution : with one array

--- Arrays_Strings/spliitArray.c
+++ Arrays_Strings/spliitArray.c
@@ -3,21 +3,15 @@
 int waysToSplitArray(int *nums, int numsSize)
 {
     long int prefix[numsSize];
-    long int suffix[numsSize];
     int i = 0, count = 0, j = numsSize - 1;
     prefix[i] = nums[i];
     for (i = 1; i < numsSize; i++)
     {
         prefix[i] = nums[i] + prefix[i - 1];
     }
-    suffix[j] = nums[j];
-    for (j = numsSize - 2; j >= 0; j--)
-    {
-        suffix[j] = nums[j] + suffix[j + 1];
-    }
     for (int k = 0; k < numsSize - 1; k++)
     {
-        if (prefix[k] >= suffix[k + 1])
+        if (prefix[k] >= (prefix[numsSize - 1] - prefix[k]))
             count++;
     }
     return (count);
