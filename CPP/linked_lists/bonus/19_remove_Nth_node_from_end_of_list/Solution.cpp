7a0dd4e9af8ca1d27b630a7ef58e544da1bf46dd
ThiwankaS <thivankas@outlook.com>
2026-03-26 03:31:02 +0200

    Removing the Nth node from a linked list starting from the end

--- CPP/linked_lists/bonus/19_remove_Nth_node_from_end_of_list/Solution.cpp
+++ CPP/linked_lists/bonus/19_remove_Nth_node_from_end_of_list/Solution.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 ListNode* Solution::removeNthFromEnd(ListNode* head, int n) {
-    
+    // helper funtion to reverse a linked list    
     auto reverse_list = [&](ListNode* node) {
         ListNode* ptr = node;
         ListNode* previous = nullptr;
@@ -17,24 +17,32 @@ ListNode* Solution::removeNthFromEnd(ListNode* head, int n) {
         return (previous);
     }; 
 
+    // reverse the original linked list
     ListNode* reversed = reverse_list(head);
     ListNode* newHead = reversed;
     ListNode* oneBefore = nullptr;
-
+    
+    // selecting the Nth node from the end
     for(int i = 1; i < n && newHead; i++) {
         oneBefore = newHead;
         newHead = newHead->next;
     }
 
+    // removing the node
     if(newHead) {
+        // storing the next node just one after the particular node
         ListNode* oneAfter = newHead->next;
         if(oneBefore) {
+            // if the node need to remove is not the head of the revered list 
+            // of Nth is not the last node
             oneBefore->next = oneAfter;
         } else {
+            // if the node need to remove is the last node, then set the head to the next one
             reversed = oneAfter;
         }
+        // relese the resource of the selected node
         delete newHead;
     }
-
+    // returning the orginal lined list
     return (reverse_list(reversed));
 }
