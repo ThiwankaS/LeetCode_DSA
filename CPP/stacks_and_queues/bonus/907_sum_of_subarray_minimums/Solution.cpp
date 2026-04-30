5097eb0ad75a4bda46dbc5fc0a845a4a3779a77f
ThiwankaS <thivankas@outlook.com>
2026-04-30 06:44:22 +0300

    calculate the sum of min(b) where b is a contageous sub-array

--- CPP/stacks_and_queues/bonus/907_sum_of_subarray_minimums/Solution.cpp
+++ CPP/stacks_and_queues/bonus/907_sum_of_subarray_minimums/Solution.cpp
@@ -9,21 +9,32 @@ int Solution::sumSubarraysMins(const std::vector<int>& arr) {
     std::stack<int> trace;
 
 
-   for(int i = 0; i <= size; ++i) {
-       int currentVal = (i == size) ? -1 : arr[i];
+    for(int i = 0; i <= size; ++i) {
 
-       while(!trace.empty() && arr[trace.top()] > currentVal ) {
-           int target = trace.top();
-           trace.pop();
+        // dummy value to prevent out of bound access
+        // will make sure stack will clear out
+        int currentVal = (i == size) ? -1 : arr[i];
 
-           int right = i;
-           int left  = trace.empty() ? -1 : trace.top();
+        // when find a value less than arr[top] then top is no loger pointing to the min value
+        while(!trace.empty() && arr[trace.top()] > currentVal ) {
+            int targetIDx = trace.top();
+            trace.pop();
+        
+            // how far to the right te new minimum from last
+            int rightDistance = i - targetIDx;
 
-           long long count = (target - left) * (right - target);
+            // now stack top is pointing to the next min
+            // if stack is empty, means currentVal is the minimum all the way from the start
+            int leftLimit  = trace.empty() ? -1 : trace.top();
+            int leftDistance = targetIDx - leftLimit;
 
-           sum = (sum + (count * arr[target])) % MOD;
-       }
-       trace.push(i);
+            // contibution : (left choices) * (right choices) * value
+            long long contribution = (long long) leftDistance * rightDistance * arr[targetIDx];
+            sum = (sum + contribution) % MOD;
+        }
+        if( i < size) {
+            trace.push(i);
+        }
    }
    return sum;
 }
